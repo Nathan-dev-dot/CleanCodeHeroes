e778462d2dbaee190e21e57e7cab22689fa1815e
Sarah <sschlegel@myges.fr>
2022-12-03 20:59:41 +0100

    initial commit

---
+++ src/main/java/com/cleancodeheroes/CleanCodeHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.cleancodeheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeHeroesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanCodeHeroesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cleancodeheroes/CleanCodeHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleancodeheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodeHeroesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
