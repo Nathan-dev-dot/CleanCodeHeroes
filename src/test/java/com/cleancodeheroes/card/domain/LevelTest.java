03f79b69b5932dafce9df13c98c324fd38f2affe
Sarah <sschlegel@myges.fr>
2023-02-19 12:20:55 +0100

    add level > 0 test and verification

--- src/test/java/com/cleancodeheroes/card/domain/LevelTest.java
+++ src/test/java/com/cleancodeheroes/card/domain/LevelTest.java
@@ -6,19 +6,25 @@ import org.junit.jupiter.api.Test;
 public class LevelTest {
     @Test
     public void shouldReturnAnLevelObject(){
-        Level level = new Level(1);
+        Level level = Level.of(1);
         Assertions.assertEquals(Level.class, level.getClass());
     }
+
+    @Test
+    public void shouldThrowIfLevelInferiorToOne () {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Level.of(0));
+    }
+
     @Test
     public void shouldReturnLevelObjectWithValue1(){
-        Level level = new Level(1);
+        Level level = Level.of(1);
         Integer newLevelInteger = level.getLevel();
         Assertions.assertEquals(1, newLevelInteger);
     }
 
     @Test
     public void shouldAddOneLevel(){
-        Level level = new Level(1);
+        Level level = Level.of(1);
         Integer newLevelInteger = level.addOneLevel().getLevel();
         Assertions.assertEquals(2, newLevelInteger);
     }
