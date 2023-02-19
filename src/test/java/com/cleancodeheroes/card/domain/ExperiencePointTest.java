79624edee14c2ba04424383965ae78b11dadea99
Sarah <sschlegel@myges.fr>
2023-02-19 14:04:48 +0100

    add experience point always positive test and verification

--- src/test/java/com/cleancodeheroes/card/domain/ExperiencePointTest.java
+++ src/test/java/com/cleancodeheroes/card/domain/ExperiencePointTest.java
@@ -6,19 +6,24 @@ import org.junit.jupiter.api.Test;
 public class ExperiencePointTest {
     @Test
     public void shouldReturnAnExperiencePointObject(){
-        ExperiencePoint experiencePoint = new ExperiencePoint(0);
+        ExperiencePoint experiencePoint = ExperiencePoint.of(0);
         Assertions.assertEquals(ExperiencePoint.class, experiencePoint.getClass());
+    }
+
+    @Test
+    public void shouldThrowIfExperiencePointIsNegative () {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> ExperiencePoint.of(-1));
     }
     @Test
     public void shouldReturnExperiencePointObjectWithValue1(){
-        ExperiencePoint experiencePoint = new ExperiencePoint(1);
+        ExperiencePoint experiencePoint = ExperiencePoint.of(1);
         Integer newExperiencePoint = experiencePoint.getExperiencePoint();
         Assertions.assertEquals(1, newExperiencePoint);
     }
 
     @Test
     public void shouldAddTenPercentOdExperiencePoint(){
-        ExperiencePoint experiencePoint = new ExperiencePoint(100);
+        ExperiencePoint experiencePoint = ExperiencePoint.of(100);
         Integer newExperiencePoint = experiencePoint.increaseByFactor().getExperiencePoint();
         Assertions.assertEquals(110, newExperiencePoint);
     }
@@ -26,21 +31,21 @@ public class ExperiencePointTest {
 
     @Test
     public void shouldAddTenPercentOdExperiencePointWithoutDecimalValue(){
-        ExperiencePoint experiencePoint = new ExperiencePoint(99);
+        ExperiencePoint experiencePoint = ExperiencePoint.of(99);
         Integer newExperiencePoint = experiencePoint.increaseByFactor().getExperiencePoint();
         Assertions.assertEquals(108, newExperiencePoint);
     }
 
     @Test
     public void shouldLevelUpReturnTrue(){
-        ExperiencePoint experiencePoint = new ExperiencePoint(5);
+        ExperiencePoint experiencePoint = ExperiencePoint.of(5);
         boolean shouldLevelUp = experiencePoint.shouldLevelUp();
         Assertions.assertEquals(true, shouldLevelUp);
     }
 
     @Test
     public void shouldLevelUpReturnFalse(){
-        ExperiencePoint experiencePoint = new ExperiencePoint(6);
+        ExperiencePoint experiencePoint = ExperiencePoint.of(6);
         boolean shouldLevelUp = experiencePoint.shouldLevelUp();
         Assertions.assertEquals(false, shouldLevelUp);
     }
