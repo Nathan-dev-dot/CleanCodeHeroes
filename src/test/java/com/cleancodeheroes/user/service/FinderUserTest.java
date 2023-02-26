f8fb6943d16e664eee957974e8d6329cdabbee23
Sarah <sschlegel@myges.fr>
2023-02-26 22:53:51 +0100

    user finder tests

---
+++ src/test/java/com/cleancodeheroes/user/service/FinderUserTest.java
@@ -0,0 +1,50 @@
+package com.cleancodeheroes.user.service;
+
+import com.cleancodeheroes.user.adapter.out.UserNotFoundException;
+import com.cleancodeheroes.user.application.port.in.FindUserQuery;
+import com.cleancodeheroes.user.application.port.out.FindUserPort;
+import com.cleancodeheroes.user.application.services.FindUserService;
+import com.cleancodeheroes.user.domain.User;
+import com.cleancodeheroes.user.domain.UserId;
+import com.cleancodeheroes.user.domain.UserProps;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class FinderUserTest {
+
+    @InjectMocks
+    FindUserService finderUserService;
+
+    @Mock
+    FindUserPort findUserPort;
+
+    @Test
+    public void shouldFindUser () {
+        User user = new User(new UserProps("Test"));
+        UserId userId = user.getUserId();
+        try {
+            when(findUserPort.load(userId)).thenReturn(user);
+            User actual = this.finderUserService.handle(new FindUserQuery(userId.value()));
+            Assertions.assertEquals(user, actual);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    @Test
+    public void shouldNotFindUser () {
+        UserId userId = new UserId();
+        try {
+            when(findUserPort.load(userId)).thenThrow(new UserNotFoundException());
+            Assertions.assertThrows(UserNotFoundException.class, () -> this.finderUserService.handle(new FindUserQuery(userId.value())));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
