951c62deef923c8096cf2dda7232e42bae8a8b1f
Nathan-dev-dot <maerna1@gmail.com>
2023-02-26 22:57:13 +0100

    BattleCreationServiceTest

--- src/test/java/com/cleancodeheroes/battle/appllication/service/BattleCreationServiceTest.java
+++ src/test/java/com/cleancodeheroes/battle/appllication/service/BattleCreationServiceTest.java
@@ -1,24 +1,77 @@
 package com.cleancodeheroes.battle.appllication.service;
 
 import com.cleancodeheroes.battle.adapter.out.NoSQLBattlePersistence;
+import com.cleancodeheroes.battle.application.port.in.CreateBattleCommand;
+import com.cleancodeheroes.battle.application.port.out.CreateBattlePort;
 import com.cleancodeheroes.battle.application.service.BattleCreationService;
+import com.cleancodeheroes.battle.domain.Battle;
+import com.cleancodeheroes.battle.domain.BattleId;
+import com.cleancodeheroes.battle.domain.BattleResult;
+import com.cleancodeheroes.card.domain.Card;
+import com.cleancodeheroes.card.domain.CardProps;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.mockito.quality.Strictness.LENIENT;
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = LENIENT)
 public class BattleCreationServiceTest {
 
     @InjectMocks
     private BattleCreationService battleCreationService;
 
     @Mock
-    NoSQLBattlePersistence noSQLBattlePersistence;
+    CreateBattlePort createBattlePort;
 
+    @Test
+    public void shouldReturnABattleId(){
+        BattleId battleId = BattleId.of(new ObjectId().toString());
+        when(this.createBattlePort.save(any(BattleResult.class))).thenReturn(battleId);
+        Card attacker = new Card(
+                new CardProps(
+                        new ObjectId().toString(),
+                        new ObjectId().toString(),
+                        "nathan",
+                        100,
+                        1,
+                        101,
+                        100,
+                        "Assassin",
+                        "Common",
+                        1,
+                        new ObjectId().toString()
+                )
+        );
+
+        Card defender = new Card(
+                new CardProps(
+                        new ObjectId().toString(),
+                        new ObjectId().toString(),
+                        "Sarah",
+                        50,
+                        1,
+                        150,
+                        100,
+                        "Assassin",
+                        "Common",
+                        1,
+                        new ObjectId().toString()
+                )
+        );
+
+        Battle battle = Battle.of(attacker, defender);
+        CreateBattleCommand createBattleCommand = new CreateBattleCommand(battle);
+        BattleId newBattleId = this.battleCreationService.handle(createBattleCommand);
+        Assertions.assertEquals(newBattleId.getClass(), BattleId.class);
+        Assertions.assertEquals(newBattleId.value(), battleId.value());
+    }
 
 }
